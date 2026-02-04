4a1a1034e9256ead767a7c94f6267ec6a8af67fb
邓思婷 <262457384@qq.com>
2026-02-04 20:51:58 +0800

    完成了商品列表的开发

--- uni-shop/subpkg/goods_list/goods_list.vue
+++ uni-shop/subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		goods_list
+		<view class="goods-list">
+			<view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+			   <my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +12,65 @@
 	export default {
 		data() {
 			return {
-				
+				//请求参数对象
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize:10
+				},
+				goodsList: [],
+				total: 0,
+				//节流阀
+				isLoading: false
 			};
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			//调用获取列表数据的方法
+			this.getGoodsList()
+		},
+		methods: {
+			async getGoodsList(cb) {
+				//打开节流阀
+				this.isLoading = true
+				const { data: res } = await uni.$http.get('api/public/v1/goods/search', this.queryObj)
+				//关闭节流阀
+				this.isLoading = false
+				//只要数据请求完毕，就立即按需调用cb回调函数
+				cb && cb()
+				
+				if(res.meta.status !== 200) return uni.$showMsg()
+				
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoDetail(goods) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				})
+		    },
+		},
+		onReachBottom() {
+			if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕')
+			
+			//判断是否正在请求
+			if(this.isLoading) return
+			
+			//让页面值自增+1
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			//1.重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isLoading = false
+			this.goodsList = []
+			
+			//2.重新发送请求
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
 	}
 </script>
